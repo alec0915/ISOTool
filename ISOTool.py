59f27c04ebdeacc7db0a646308efb955667d58b8
abwhyte <abwhyte@uci.edu>
2025-12-29 15:06:21 -0800

    Started ISOTool.py. Can properly copy files into new folders; must be name consistent and doesn't work for 2 discs

---
+++ ISOTool.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import os
+import shutil
+
+folder_path = Path(os.getcwd())
+
+def makeFolder(directory,gameName):
+    name = (gameName.split('.')[0])
+    newFolder = str(folder_path) + '\\'+name
+    if name != 'game':
+        try:
+            print("Creating folder for", name)
+            os.makedirs(newFolder)
+        except FileExistsError:
+            print(name, "folder already exists")
+        if Path(newFolder+'\\*.iso').is_file():
+            print('Renaming file to game.iso')
+            os.rename(newFolder+'\\'+name+'.iso',newFolder+'\\game.iso')
+        elif Path(newFolder+'\\game.iso').is_file():
+            print(name,"exists as game.iso") 
+        else:
+            print("Copying file")
+            shutil.copy2(str(folder_path)+'\\'+ gameName,newFolder)
+            if Path(newFolder+'\\'+name+'.iso').is_file():
+                print('Renaming file to game.iso')
+                os.rename(newFolder+'\\'+name+'.iso',newFolder+'\\game.iso')
+
+
+
+
+
+for i in folder_path.glob('**/*.iso'):  #iterating through paths
+    gamename=str(i).split('\\')[-1]       #extracted file name
+    directory=i
+    makeFolder(directory,gamename)
+
